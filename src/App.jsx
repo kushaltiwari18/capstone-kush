7fa7a9681729dfb78eb07eb0f483f4312dc4d07f
kushaltiwari18 <kushaltiwari18@gmail.com>
2021-12-30 03:50:50 +0530

    adding field email & mob

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 
 const App1 = () => {
-  const [name, setName] = useState("");
-  const [fullName, setFullName] = useState();
-  const [lastname, setLastName] = useState();
-  const [lastNameNew, setLastNameNew] = useState("");
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    mob:"",
+  });
 
   const onSubmits = (event) => {
     event.preventDefault();
-    setFullName(name);
-    setLastNameNew(lastname);
+    alert("Form submitted");
   };
 
   const inputEvent = (e) => {
-    console.log(e.target.value);
-    setName(e.target.value);
-  };
+    // console.log(e.target.value);
+    // console.log(e.target.name);
 
-  const inputEventTwo = (event) => {
-    setLastName(event.target.value);
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFullName((preValue) => {
+      // console.log(preValue);
+      if (name === "fName") {
+        return { fname: value, lname: preValue.lname ,email:preValue.email,mob:preValue.mob};
+      } else if (name === "lName") {
+        return { fname: preValue.fname, lname: value ,email:preValue.email,mob:preValue.mob};
+      }else if (name === "Email") {
+        return { fname: preValue.fname, lname: preValue.lname ,email: value ,mob:preValue.mob};
+      }else if (name === "Mob") {
+        return { fname: preValue.fname, lname: preValue.lname, email:preValue.email,mob: value };
+      }
+    });
   };
 
   return (
@@ -26,22 +39,46 @@ const App1 = () => {
       <div>
         <form onSubmit={onSubmits}>
           <h1>
-            Hello {fullName} {lastNameNew}
+            Hello {fullName.fname} {fullName.lname}
           </h1>
+          <p>{fullName.email}</p>
+          <p>{fullName.mob}</p>
           <input
             type="text"
             placeholder="Enter Your first name"
+            name="fName"
             onChange={inputEvent}
-            value={name}
+            value={fullName.fname}
+            autoComplete="off"
           />
           <br />
           <input
             type="text"
             placeholder="Enter Your last name"
-            onChange={inputEventTwo}
-            value={lastname}
+            name="lName"
+            onChange={inputEvent}
+            value={fullName.lname}
+            autoComplete="off"
           />
-
+          <br/>
+            <input
+            type="text"
+            placeholder="Enter Your Email"
+            name="Email"
+            onChange={inputEvent}
+            value={fullName.email}
+            autoComplete="off"
+          />
+          <br/> 
+          <input
+          type="text"
+          placeholder="Enter Your Mob no."
+          name="Mob"
+          onChange={inputEvent}
+          value={fullName.mob}
+          autoComplete="off"
+        />
+        <br/>
           <button type="submit">Click Me👍</button>
         </form>
       </div>
